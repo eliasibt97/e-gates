4ef76ae54fd2123a7426dd6f627d726ab0467498
Eliasib Toris <manuel.calles@kavak.com>
2022-03-08 12:00:40 -0700

    fix: allow other boolean types

--- app/utils/bool.ts
+++ app/utils/bool.ts
@@ -1,1 +1,15 @@
-export const getBoolValue = (value: string): boolean => value === 'true' ? true : false;
+export const getBoolValue = (value: string): boolean => {
+    let input = false;
+    value = value.toLowerCase();
+    switch (value) {
+        case "true":
+        case "1":
+        case "on":
+            input = true;
+            break;
+        default:
+            input = false;
+            break;
+    }
+    return input;
+};
